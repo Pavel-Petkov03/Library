cfdcd5d514e81e34a4ff37819d143f061074acd7
Pavel Petkov <elias5247698@abv.bg>
2022-02-17 23:53:10 +0200

    making manual changes

--- Library/main/views/home_page.py
+++ Library/main/views/home_page.py
@@ -10,8 +10,8 @@ class HomeView(View):
     def get(self, req):
         if check_if_has_profile():
             books = Book.objects.all()
-            return render(req, "home-with-profile.html",{
-                "books" : books
+            return render(req, "home-with-profile.html", {
+                "books": books
             })
         form = CreateProfileForm()
         return render(req, "home-no-profile.html", {
